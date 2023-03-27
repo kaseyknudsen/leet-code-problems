22235e269feff8e8d91a0373fdf10a1416b19c26
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-26 23:06:58 -0700

    solution 1

--- goodPairs.js
+++ goodPairs.js
@@ -1,4 +1,33 @@
+//solution 1
 const GoodPairs = () => {
-  const arr = [1,2,4,3,5,6,1,3]
+  const arr = [1, 2, 4, 3, 5, 6, 1, 3];
+  let pairs = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        pairs++;
+      }
+    }
+  }
+  console.log(pairs);
 };
-console.log(GoodPairs());
+GoodPairs();
+
+// const GoodPairs = (nums) => {
+//   const freqCounter = {};
+//   let pairs = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (freqCounter[nums[i]]) {
+//       freqCounter[nums[i]] += 1;
+//     } else {
+//       freqCounter[nums[i]] = 1;
+//     }
+//   }
+//   const values = Object.values(freqCounter);
+//   values.forEach((value) => {
+//     value > 1 ? (pairs += 1) : null;
+//   });
+//   return pairs;
+// };
+
+
