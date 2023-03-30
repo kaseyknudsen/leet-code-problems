61635b18711beef347b90f49a6b1001949e75dc2
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-29 20:05:56 -0700

    hash map solution

--- longestSubstring.js
+++ longestSubstring.js
@@ -24,22 +24,71 @@ and we're looking for a subset of that data that is continuous*/
 //   return maxSum;
 // }
 
-const maxSubArraySum = (arr, num) => {
-  //create 2 variables for the maximum sub and the temporary sum
-  let maxSum = 0;
-  let tempSum = 0;
-  //create the first window
-  for (let i = 0; i < num; i++) {
-    maxSum += arr[i];
+// const maxSubArraySum = (arr, num) => {
+//   //create 2 variables for the maximum sub and the temporary sum
+//   let maxSum = 0;
+//   let tempSum = 0;
+//   //create the first window
+//   for (let i = 0; i < num; i++) {
+//     maxSum += arr[i];
+//   }
+//   //make the tempSum = maxSum
+//   tempSum = maxSum;
+//   //create the code that takes the temporary sum, subtracts the 1st num and adds the next num
+//   for (let i = num; i < arr.length; i++) {
+//     tempSum = tempSum - arr[i - num] + arr[i];
+//     maxSum = Math.max(maxSum, tempSum);
+//   }
+//   return maxSum;
+// };
+
+// console.log(maxSubArraySum([1, 2, 3, 4, 3, 2, 1], 3));
+
+//brute force method. Bad.
+const isUnique = (s) => {
+  const set = new Set();
+  for (let i = 0; i < s.length; i++) {
+    if (set.has(s[i])) {
+      return false;
+    }
+    set.add(s[i]);
   }
-  //make the tempSum = maxSum
-  tempSum = maxSum;
-  //create the code that takes the temporary sum, subtracts the 1st num and adds the next num
-  for (let i = num; i < arr.length; i++) {
-    tempSum = tempSum - arr[i - num] + arr[i];
-    maxSum = Math.max(maxSum, tempSum);
-  }
-  return maxSum;
+  return true;
 };
 
-console.log(maxSubArraySum([1, 2, 3, 4, 3, 2, 1], 3));
+// const longestSubString = (s) => {
+//   let max = 0;
+//   for (let begin = 0; begin < s.length; begin++) {
+//     for (let end = begin; end < s.length; end++) {
+//       //this will give us all possible substrings
+//       const substring = s.substring(begin, end + 1);
+//       if (isUnique(substring)) {
+//         max = Math.max(max, substring.length);
+//       }
+//     }
+//   }
+//   return max;
+// };
+
+// console.log(longestSubString("abcacbacb"));
+
+//optimal solutions
+//hash map
+const longestSubString = (s) => {
+  let max = 0;
+  let begin = 0;
+  let map = {};
+  for (let end = 0; end < s.length; end++) {
+    if (map[s[end]] !== undefined && map[s[end]] >= begin) {
+      begin = map[s[end]] + 1;
+    }
+    map[s[end]] = end;
+    max = Math.max(max, end - begin + 1);
+  }
+  return max;
+};
+
+
+console.log(longestSubString("abcacbacb"));
+
+//sliding window set solution
