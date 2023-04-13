87521e54f3a3d0081c2e592a714b774dbac21a6d
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-12 22:33:17 -0700

    longeset substring

--- longestSubstring.js
+++ longestSubstring.js
@@ -15,7 +15,9 @@ function maxSubArraySum(arr, num) {
   tempSum = maxSum; //6
 
   for (let i = num; i < arr.length; i++) {
-    console.log(`tempSum: ${tempSum}, - arr[i-num]: ${arr[i - num]}, + arr[i]: ${arr[i]}`);
+    console.log(
+      `tempSum: ${tempSum}, - arr[i-num]: ${arr[i - num]}, + arr[i]: ${arr[i]}`
+    );
     tempSum = tempSum - arr[i - num] + arr[i];
     maxSum = Math.max(maxSum, tempSum);
     console.log(`maxSum: ${maxSum}`);
@@ -23,4 +25,37 @@ function maxSubArraySum(arr, num) {
   return maxSum;
 }
 
-console.log(maxSubArraySum([1, 2, 3, 4, 3, 2, 1], 3));
+// console.log(maxSubArraySum([1, 2, 3, 4, 5, 6], 3));
+
+const lengthOfLongestSubstring = (s) => {
+  let max = 0;
+  let startIndex = 0;
+  const charCount = {};
+
+  for (let endIndex = 0; endIndex < s.length; endIndex++) {
+    let rightChar = s[endIndex];
+    charCount[rightChar] = charCount[rightChar] + 1 || 1;
+
+    while (charCount[rightChar] > 1) {
+      console.log("We have entered the while loop!")
+      let leftChar = s[startIndex];
+      console.log(`left char: ${leftChar}`)
+      if (charCount[leftChar] > 1) {
+        console.log(`${charCount[leftChar]} is greater than 1.`)
+        charCount[leftChar]--;
+      } else {
+        console.log(`Deleting leftChar: ${leftChar}`)
+        delete charCount[leftChar];
+      }
+      startIndex++;
+    }
+    max = Math.max(max, (endIndex - startIndex) + 1);
+    console.log(`max: ${max}`)
+    console.log("charCount:")
+    console.log(charCount)
+    // console.log(`charCount: ${JSON.stringify(charCount)}`)
+  }
+  return max;
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'))
