7dbc933f090ac0234f60e282b148bec64b23333c
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-14 12:25:00 -0700

    sliding window longest substring final answer

--- longestSubstringFinalAnswer.js
+++ longestSubstringFinalAnswer.js
@@ -2,38 +2,23 @@ const longestSubString3 = (s) => {
   let max = 0;
   let freqCounter = {};
   let left = 0;
-
   for (let right = 0; right < s.length; right++) {
     let rightChar = s[right];
     freqCounter[rightChar] = (freqCounter[rightChar] ?? 0) + 1;
-    if (freqCounter[rightChar] > 1) {
-      max = Math.max(max, right - left);
-    }
+
     while (freqCounter[rightChar] > 1) {
       leftChar = s[left];
       if (freqCounter[leftChar] > 1) {
         freqCounter[leftChar]--;
+      } else {
+        delete freqCounter[leftChar];
       }
       left++;
     }
+    max = Math.max(max, right - left + 1);
+    console.log(`max: ${max}`);
   }
   return max;
 };
 
-console.log(longestSubString3("abcbaef"));
-
-/* right = 0
-rightChar = a
-a: 1
-right = 1
-rightChar = b
-b: 1
-right = 2
-rightChar = c
-c: 1
-right = 3
-rightChar = b
-b: 2
-max is equal to 3 - 0
-
-*/
+console.log(longestSubString3("tmmzuxt"));
