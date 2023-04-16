82856f353f2001e4c142e90e7f581f9e1d82959c
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-15 22:49:04 -0700

    notes on 3sum

--- 3Sum.js
+++ 3Sum.js
@@ -2,3 +2,10 @@
 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that 
 i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 */
+/* 
+nums[i]
+nums[j]
+nums[k]
+
+input [-1, 0, 1, 2, -1, -4]
+*/
