d0c98b6f378302b9f2a36b20df96abb45acb84c1
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-18 13:53:58 -0700

    still working out 2Sum

--- 2Sum.js
+++ 2Sum.js
@@ -10,15 +10,20 @@ element twice
 
 const sum2 = (inputArray, target, nums) => {
   const pairsArray = [];
-  const pairsObj = {};
-  const sortedArray = inputArray.sort((a, b) => a - b);
-  for (let i = 0; i <= sortedArray.length - nums; i++) {
-    for (let j = i + 1; j < sortedArray.length; j++) {
-      let pairs = [sortedArray[i], sortedArray[j]];
-      if (sortedArray[i] + sortedArray[j] === target) {
-        pairsObj[pairs] = (pairsObj[pairs] || 0) + 1;
-        if (pairsObj[pairs] < 2) {
-          pairsArray.push(pairs);
+  const freqCounter = {};
+  const sortedInputArray = inputArray
+    .map((value, index) => {
+      value, index;
+      console.log(value, index)
+    })
+    .sort((a, b) => a.value - b.value);
+  for (let i = 0; i <= inputArray.length - nums; i++) {
+    for (let j = i + 1; j < inputArray.length; j++) {
+      if (sortedInputArray[i] + sortedInputArray[j] === target) {
+        let pairs = [sortedInputArray[i], sortedInputArray[j]];
+        freqCounter[pairs] = (freqCounter[pairs] || 0) + 1;
+        if (freqCounter[pairs] < 2) {
+          pairsArray.push([i, j]);
         }
       }
     }
@@ -27,3 +32,4 @@ const sum2 = (inputArray, target, nums) => {
 };
 
 console.log(sum2([1, 3, 1, 2, 2], 4, 2));
+//should return ([0, 1], [1, 2], [3, 4])
---
+++ practice.js
@@ -0,0 +1,12 @@
+const array = [1, 3, 1, 2, 2];
+//map function is creating an array of objects
+//each object has a value and it's associated index
+const sortedArray = array
+  .map((value, index) => ({
+    value,
+    index,
+  }))
+  //the sort method is sorting each object by it's value
+  .sort((a, b) => a.value - b.value);
+
+console.log(sortedArray);
