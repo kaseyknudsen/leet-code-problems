d7a07654dc41b74b900b42679db831ec34e2ffa1
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-18 22:35:34 -0700

    more notes on 2Sum problem

--- 2Sum.js
+++ 2Sum.js
@@ -24,3 +24,29 @@ const sum2 = (inputArray, target) => {
 };
 
 console.log(sum2([1, 2, 4, 5, 7, 3], 6));
+
+/* 
+compliment number = 6 - 1 = 5
+{
+  1: 0
+}
+compliment number = 6 - 2 = 4
+{
+  1: 0
+  2: 1
+}
+compliment number = 6 - 4 = 2
+{
+  1: 0
+  2: 1 -- we have a match!
+}
+push 1, 2 to the array
+
+compliment number = 6 - 5 = 1
+there is a 1 in the hashMap, so we push 0 & 3 to the array
+{
+  1: 0
+  2: 1 -- we have a match!
+
+}
+*/
