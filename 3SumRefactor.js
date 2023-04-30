62bc050b88a6a183115e9453c99abb3dd576b8f3
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-29 22:19:58 -0700

    cleaned up console logs and comments for 3Sum

--- 3SumRefactor.js
+++ 3SumRefactor.js
@@ -1,7 +1,5 @@
 const sum3 = (arr) => {
   arr.sort((a, b) => a - b);
-  //[-3, -1, -1, 0, 0,  1,  1, 2]
-
   const zeroSumArray = [];
   for (let i = 0; i < arr.length - 2; i++) {
     if (i > 0 && arr[i] === arr[i - 1]) {
@@ -36,52 +34,3 @@ const sum3 = (arr) => {
 };
 
 console.log(sum3([-1, 0, 1, 2, -3, -1, 0, 1]));
-
-// const sum3 = (arr) => {
-//   arr.sort((a, b) => a - b);
-//   //[-3, -1, -1, 0, 0,  1,  1, 2]
-//   const zeroSumArray = [];
-//   for (let i = 0; i < arr.length - 2; i++) {
-//     let target = arr[i] * -1;
-//     if (i > 0 && arr[i] === arr[i - 1]) {
-//       continue;
-//     }
-//     for (let j = i + 1; j < arr.length - 1; j++) {
-//       if (j > i + 1 && arr[j] === arr[j - 1]) {
-//         continue;
-//       }
-//       let k = arr.length - 1;
-//       while (k > j) {
-//         if (k < arr.length - 1 && arr[k] === arr[k + 1]) {
-//           k--;
-//         } else if (arr[j] + arr[k] < target) {
-//           j++;
-//         } else if (arr[j] + arr[k] > target) {
-//           k--;
-//         } else {
-//           zeroSumArray.push([target * -1, arr[j], arr[k]]);
-//           j++;
-//           k--;
-//         }
-//       }
-//     }
-//   }
-
-//   return zeroSumArray;
-// };
-
-//console.log(sum3([-1, 0, 1, 2, -3, -1, 0, 1]));
-//create a hashmap:
-/*
- const hashMap = {
-  i: {
-    '-3': 1
-  }
-  j: {
-    '-1': 1
-  }
-  k: {
-    '2': 1
-  }
- }
- */
