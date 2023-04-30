0107b180df2aa848f8f02557fcdd95e29362e107
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-29 22:19:05 -0700

    solution finally accepted by leetcode

--- 3SumRefactor.js
+++ 3SumRefactor.js
@@ -1,86 +1,78 @@
 const sum3 = (arr) => {
   arr.sort((a, b) => a - b);
   //[-3, -1, -1, 0, 0,  1,  1, 2]
-  const hashMapI = {};
+
   const zeroSumArray = [];
   for (let i = 0; i < arr.length - 2; i++) {
-    let target = arr[i] * -1;
-    hashMapI[target] = (hashMapI[target] || 0) + 1;
-    console.log(`I:`)
-    console.log(hashMapI);
-    if (hashMapI[target] > 1) {
-      console.log('yes')
+    if (i > 0 && arr[i] === arr[i - 1]) {
       continue;
     }
+    let target = arr[i] * -1;
     for (let j = i + 1; j < arr.length - 1; j++) {
-      const hashMapJ = {}
-      hashMapJ[arr[j]] = (hashMapJ[arr[j]] || 0) + 1;
-      console.log(`J:`)
-      console.log(hashMapJ);
-      if (hashMapJ[arr[j]] > 1) {
-        continue;
-      }
       let k = arr.length - 1;
       while (k > j) {
-        mapK = {}
-        mapK[arr[k]] = (mapK[arr[k]] || 0) + 1;
-        console.log(mapK);
-        if (mapK[arr[k]] > 1) {
-          k--;
-        } else if (arr[j] + arr[k] < target) {
+        let leftValue = arr[j];
+        let rightValue = arr[k];
+        if (leftValue + rightValue < target) {
           j++;
-        } else if (arr[j] + arr[k] > target) {
+        } else if (leftValue + rightValue > target) {
           k--;
         } else {
-          zeroSumArray.push([target * -1, arr[j], arr[k]]);
+          zeroSumArray.push([target * -1, leftValue, rightValue]);
+          while (j < k && arr[j] === arr[j + 1]) {
+            j++;
+          }
+          while (j < k && arr[k] === arr[k - 1]) {
+            k--;
+          }
           j++;
           k--;
         }
       }
     }
   }
- 
+
   return zeroSumArray;
- };
- 
- console.log(sum3([-1, 0, 1, 2, -3, -1, 0, 1]));
- 
- // const sum3 = (arr) => {
- //   arr.sort((a, b) => a - b);
- //   //[-3, -1, -1, 0, 0,  1,  1, 2]
- //   const zeroSumArray = [];
- //   for (let i = 0; i < arr.length - 2; i++) {
- //     let target = arr[i] * -1;
- //     if (i > 0 && arr[i] === arr[i - 1]) {
- //       continue;
- //     }
- //     for (let j = i + 1; j < arr.length - 1; j++) {
- //       if (j > i + 1 && arr[j] === arr[j - 1]) {
- //         continue;
- //       }
- //       let k = arr.length - 1;
- //       while (k > j) {
- //         if (k < arr.length - 1 && arr[k] === arr[k + 1]) {
- //           k--;
- //         } else if (arr[j] + arr[k] < target) {
- //           j++;
- //         } else if (arr[j] + arr[k] > target) {
- //           k--;
- //         } else {
- //           zeroSumArray.push([target * -1, arr[j], arr[k]]);
- //           j++;
- //           k--;
- //         }
- //       }
- //     }
- //   }
- 
- //   return zeroSumArray;
- // };
- 
- //console.log(sum3([-1, 0, 1, 2, -3, -1, 0, 1]));
- //create a hashmap:
- /*
+};
+
+console.log(sum3([-1, 0, 1, 2, -3, -1, 0, 1]));
+
+// const sum3 = (arr) => {
+//   arr.sort((a, b) => a - b);
+//   //[-3, -1, -1, 0, 0,  1,  1, 2]
+//   const zeroSumArray = [];
+//   for (let i = 0; i < arr.length - 2; i++) {
+//     let target = arr[i] * -1;
+//     if (i > 0 && arr[i] === arr[i - 1]) {
+//       continue;
+//     }
+//     for (let j = i + 1; j < arr.length - 1; j++) {
+//       if (j > i + 1 && arr[j] === arr[j - 1]) {
+//         continue;
+//       }
+//       let k = arr.length - 1;
+//       while (k > j) {
+//         if (k < arr.length - 1 && arr[k] === arr[k + 1]) {
+//           k--;
+//         } else if (arr[j] + arr[k] < target) {
+//           j++;
+//         } else if (arr[j] + arr[k] > target) {
+//           k--;
+//         } else {
+//           zeroSumArray.push([target * -1, arr[j], arr[k]]);
+//           j++;
+//           k--;
+//         }
+//       }
+//     }
+//   }
+
+//   return zeroSumArray;
+// };
+
+//console.log(sum3([-1, 0, 1, 2, -3, -1, 0, 1]));
+//create a hashmap:
+/*
  const hashMap = {
   i: {
     '-3': 1
@@ -93,4 +85,3 @@ const sum3 = (arr) => {
   }
  }
  */
- 
